380a68cf84a31597cd60008aa6c238b07da4bad7
Kamil Jaworski <kamilj610@gmail.com>
2019-01-18 12:33:01 +0100

    Add test button that generate pdf file

--- AttendanceListGenerator/AttendanceListGenerator.UI/MainWindow.xaml.cs
+++ AttendanceListGenerator/AttendanceListGenerator.UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AttendanceListGenerator.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,12 @@ namespace AttendanceListGenerator.UI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PDFGenerator generator = new PDFGenerator();
+            generator.Generate();
         }
     }
 }
