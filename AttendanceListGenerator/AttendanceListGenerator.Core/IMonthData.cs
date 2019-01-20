351bf8bd14fde0e3274b7181549e929cf14a9a5b
Kamil Jaworski <kamilj610@gmail.com>
2019-01-20 12:30:53 +0100

    Add IMonthData interface and its class implementation

---
+++ AttendanceListGenerator/AttendanceListGenerator.Core/IMonthData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AttendanceListGenerator.Core
+{
+    public interface IMonthData
+    {
+        IList<IDay> Days { get; }
+        Month Month { get; }
+        int Year { get; }
+    }
+}
---
+++ AttendanceListGenerator/AttendanceListGenerator.Core/MonthData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AttendanceListGenerator.Core
+{
+    public class MonthData : IMonthData
+    {
+        public IList<IDay> Days { get; private set; }
+        public Month Month { get; private set; }
+        public int Year { get; private set; }
+    }
+}
