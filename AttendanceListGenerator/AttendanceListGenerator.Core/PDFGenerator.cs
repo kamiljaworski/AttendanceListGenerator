fdb65400c3aa5750e3d8a6779dac6aba8612cf5a
Kamil Jaworski <kamilj610@gmail.com>
2019-01-20 11:55:59 +0100

    Change PdfGenerator.Generate() to create a simple table

--- AttendanceListGenerator/AttendanceListGenerator.Core/PDFGenerator.cs
+++ AttendanceListGenerator/AttendanceListGenerator.Core/PDFGenerator.cs
@@ -1,4 +1,6 @@
-﻿using PdfSharp1_32.Drawing;
+﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Tables;
+using MigraDoc.Rendering;
 using PdfSharp1_32.Pdf;
 using System.Diagnostics;
 
@@ -8,28 +10,54 @@ namespace AttendanceListGenerator.Core
     {
         public void Generate()
         {
-            // Create a new PDF document
-            PdfDocument document = new PdfDocument();
+            Document document = new Document();
+            document.Info.Title = "test pdf table";
+            document.Info.Author = "Kamil Jaworski";
+            document.Info.Comment = "Some test simple table";
 
-            // Create an empty page
-            PdfPage page = document.AddPage();
-            page.Orientation = PdfSharp1_32.PageOrientation.Landscape;
-            page.Size = PdfSharp1_32.PageSize.A4;
+            // Get page setup and change the orientation
+            PageSetup setup = document.DefaultPageSetup.Clone();
+            setup.Orientation = Orientation.Landscape;
 
-            // Get an XGraphics object for drawing
-            XGraphics gfx = XGraphics.FromPdfPage(page);
+            // Add new section and set its page setup (orientation) to landscape
+            Section section = document.AddSection();
+            section.PageSetup = setup;
 
-            // Create a font
-            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+            // Create a table
+            Table table = section.AddTable();
+            table.Borders.Color = Colors.Black;
 
-            // Draw the text
-            gfx.DrawString("Hello, World!", font, XBrushes.Black,
-              new XRect(0, 0, page.Width, page.Height),
-              XStringFormats.Center);
+
+            // Add columns
+            table.AddColumn();
+            table.AddColumn();
+            table.AddColumn();
+
+            // Add rows
+            Row row = table.AddRow();
+            row.Cells[0].AddParagraph("Test 1");
+            row.Cells[1].AddParagraph("Test 2");
+            row.Cells[2].AddParagraph("Test 3");
+
+            row = table.AddRow();
+            row.Shading.Color = Colors.LightGray;
+            row.Cells[0].AddParagraph("abc");
+            row.Cells[1].AddParagraph("def");
+            row.Cells[2].AddParagraph("ghj");
+
+
+            // Saving the document
+            //string ddl = MigraDoc.DocumentObjectModel.IO.DdlWriter.WriteToString(document);
+            MigraDoc.DocumentObjectModel.IO.DdlWriter.WriteToFile(document, "MigraDoc.mdddl");
+
+            PdfDocumentRenderer renderer = new PdfDocumentRenderer(true, PdfFontEmbedding.Always);
+            renderer.Document = document;
+
+            renderer.RenderDocument();
 
             // Save the document...
-            string filename = "HelloWorld.pdf";
-            document.Save(filename);
+            string filename = "SimpleTable.pdf";
+            renderer.PdfDocument.Save(filename);
             // ...and start a viewer.
             Process.Start(filename);
         }
