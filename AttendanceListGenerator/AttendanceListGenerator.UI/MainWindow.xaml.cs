c27c6b974cc9aafe8fc5044cffd0787140fe8396
Kamil Jaworski <kamilj610@gmail.com>
2019-01-24 18:30:38 +0100

    Add temporary ILocalizedNames implementation class and pdf generator in MainWindow

--- AttendanceListGenerator/AttendanceListGenerator.UI/MainWindow.xaml.cs
+++ AttendanceListGenerator/AttendanceListGenerator.UI/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using AttendanceListGenerator.Core.Pdf;
+﻿using AttendanceListGenerator.Core.Data;
+using AttendanceListGenerator.Core.Pdf;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Tables;
+using MigraDoc.Rendering;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 
 namespace AttendanceListGenerator.UI
@@ -13,10 +19,30 @@ namespace AttendanceListGenerator.UI
             InitializeComponent();
         }
 
+        public object PdfFontEmbedding { get; private set; }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            PdfGenerator generator = new PdfGenerator();
-            generator.Generate();
+            IAttendanceListData attendanceListData = new AttendanceListData(new List<string> { "test", "test" }, Month.February, 2019);
+            IAttendanceListDocumentGenerator documentGenerator = new AttendanceListDocumentGenerator(attendanceListData, new TempLocalizedNames());
+            Document document = documentGenerator.GenerateDocument();
+            SaveDocument(document);
+        }
+
+        private void SaveDocument(Document document)
+        {
+            MigraDoc.DocumentObjectModel.IO.DdlWriter.WriteToFile(document, "MigraDoc.mdddl");
+
+            PdfDocumentRenderer renderer = new PdfDocumentRenderer(true, PdfSharp1_32.Pdf.PdfFontEmbedding.Always);
+            renderer.Document = document;
+
+            renderer.RenderDocument();
+
+            // Save the document...
+            string filename = "SimpleTable.pdf";
+            renderer.PdfDocument.Save(filename);
+            // ...and start a viewer.
+            Process.Start(filename);
         }
     }
 }
---
+++ AttendanceListGenerator/AttendanceListGenerator.UI/TempLocalizedNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AttendanceListGenerator.Core.Data;
+
+namespace AttendanceListGenerator.UI
+{
+    // TODO: Make actual LocalizedNames class
+    public class TempLocalizedNames : ILocalizedNames
+    {
+        public string DocumentAuthor => "Kamil Jaworski";
+
+        public string DocumentComment => "Lista pracownicza luty 2019";
+
+        public string GetDayOfWeekName(DayOfWeek dayOfWeek)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDocumentTitle(Month month, int Year) => "Luty 2019";
+
+        public string GetMonthName(Month month)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
