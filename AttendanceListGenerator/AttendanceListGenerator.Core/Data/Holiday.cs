513338a040033d5a203f6cde70826c7804369621
Kamil Jaworski <kamilj610@gmail.com>
2019-02-03 19:26:07 +0100

    Add Holiday enum with polish holidays

---
+++ AttendanceListGenerator/AttendanceListGenerator.Core/Data/Holiday.cs
@@ -0,0 +1,19 @@
+﻿namespace AttendanceListGenerator.Core.Data
+{
+    public enum Holiday
+    {
+        None,
+        NewYearsDay,
+        Epiphany,
+        EasterSunday,
+        EasterMonday,
+        LabourDay,
+        ConstitutionDay,
+        DescendOfTheHolySpirit,
+        CorpusChristiDay,
+        ArmedForcesDay,
+        AllSaintsDay,
+        IndependenceDay,
+        Christmas
+    }
+}
