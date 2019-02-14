6b90ad7573958549a47f7d59fe41f5576035d160
Kamil Jaworski <kamilj610@gmail.com>
2019-02-14 20:08:50 +0100

    Add MainWindowViewModel and month changing commands

--- AttendanceListGenerator/AttendanceListGenerator.UI/MainWindow.xaml.cs
+++ AttendanceListGenerator/AttendanceListGenerator.UI/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using AttendanceListGenerator.Core.Data;
-using AttendanceListGenerator.Core.IO;
-using AttendanceListGenerator.Core.Pdf;
-using MigraDoc.DocumentObjectModel;
-using MigraDoc.DocumentObjectModel.Tables;
-using MigraDoc.Rendering;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AttendanceListGenerator.UI
 {
@@ -19,36 +10,6 @@ namespace AttendanceListGenerator.UI
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        public object PdfFontEmbedding { get; private set; }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            IList<IPerson> people = new List<IPerson>
-            {
-                new Person ("James", "Hunt"),
-                new Person ("William", "Jefferson")
-            };
-
-            IAttendanceListData attendanceListData = new AttendanceListData(new DaysOffData(2019), people, Month.January, 2019);
-            ILocalizedNames localizedNames = new TempLocalizedNames();
-            IAttendanceListDocumentGenerator documentGenerator = new AttendanceListDocumentGenerator(attendanceListData, localizedNames);
-            Document document = documentGenerator.GenerateDocument();
-
-            string path = new DirectoryProvider(localizedNames).GetDocumentsDirectoryPath();
-            string filename = new FilenameGenerator(localizedNames, new DateTimeProvider()).GeneratePdfDocumentFilename(attendanceListData);
-
-            
-            if(!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
-
-            FileSaver fileSaver = new FileSaver();
-            bool fileExist = fileSaver.SavePdfDocument(document, path, filename);
-
-            if (fileExist)
-                new FileOpener().OpenFile(path, filename);
         }
     }
 }
---
+++ AttendanceListGenerator/AttendanceListGenerator.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using AttendanceListGenerator.Core.Data;
+using AttendanceListGenerator.Core.Helpers;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace AttendanceListGenerator.UI.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public Month Month { get; private set; }
+        public ICommand NextMonthCommand { get; private set; }
+        public ICommand PreviousMonthCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            Month = Month.February;
+
+            NextMonthCommand = new RelayCommand(() =>
+            {
+                Month = EnumNavigator<Month>.Next(Month);
+            });
+
+            PreviousMonthCommand = new RelayCommand(() =>
+            {
+                Month = EnumNavigator<Month>.Previous(Month);
+            });
+        }
+    }
+}
