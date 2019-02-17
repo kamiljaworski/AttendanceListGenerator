b06fd0d09024fcf21450276725eacd4a348c626e
Kamil Jaworski <kamilj610@gmail.com>
2019-02-17 19:51:16 +0100

    Add simple GenerateCommand to the MainWindowViewModel

--- src/AttendanceListGenerator.UI/ViewModels/MainWindowViewModel.cs
+++ src/AttendanceListGenerator.UI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using AttendanceListGenerator.Core.Data;
 using AttendanceListGenerator.Core.Helpers;
+using AttendanceListGenerator.Core.IO;
+using AttendanceListGenerator.Core.Pdf;
+using MigraDoc.DocumentObjectModel;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows.Input;
 
@@ -14,16 +18,24 @@ namespace AttendanceListGenerator.UI.ViewModels
 
         public Month Month { get; private set; }
         public int Year { get; private set; }
+        public string FirstPersonFullname { get; set; }
+        public string SecondPersonFullname { get; set; }
+        public string ThirdPersonFullname { get; set; }
+        public string FourthPersonFullname { get; set; }
+        public string FifthPersonFullname { get; set; }
+        public string SixthPersonFullname { get; set; }
+        public string SeventhPersonFullname { get; set; }
 
         public ICommand NextYearCommand { get; private set; }
         public ICommand PreviousYearCommand { get; private set; }
         public ICommand NextMonthCommand { get; private set; }
         public ICommand PreviousMonthCommand { get; private set; }
+        public ICommand GenerateCommand { get; private set; }
 
         public MainWindowViewModel(IDateTimeProvider dateTimeProvider)
         {
             if (dateTimeProvider == null)
-                throw new ArgumentNullException("Date Time Provider cannot be null");
+                throw new ArgumentNullException("DateTimeProvider cannot be null");
 
             _dateTimeProvider = dateTimeProvider;
             DateTime now = _dateTimeProvider.Now;
@@ -55,6 +67,57 @@ namespace AttendanceListGenerator.UI.ViewModels
                     Year--;
 
                 Month = Month.Previous();
+            });
+
+            GenerateCommand = new RelayCommand(() =>
+            {
+                IList<IPerson> people = new List<IPerson>();
+
+                if (!string.IsNullOrEmpty(FirstPersonFullname))
+                    people.Add(new Person(FirstPersonFullname));
+
+                if (!string.IsNullOrEmpty(SecondPersonFullname))
+                    people.Add(new Person(SecondPersonFullname));
+
+                if (!string.IsNullOrEmpty(ThirdPersonFullname))
+                    people.Add(new Person(ThirdPersonFullname));
+
+                if (!string.IsNullOrEmpty(FourthPersonFullname))
+                    people.Add(new Person(FourthPersonFullname));
+
+                if (!string.IsNullOrEmpty(FifthPersonFullname))
+                    people.Add(new Person(FifthPersonFullname));
+
+                if (!string.IsNullOrEmpty(SixthPersonFullname))
+                    people.Add(new Person(SixthPersonFullname));
+
+                if (!string.IsNullOrEmpty(SeventhPersonFullname))
+                    people.Add(new Person(SeventhPersonFullname));
+
+                // Generate data
+                IDaysOffData daysOff = new DaysOffData(Year);
+                IAttendanceListData listData = new AttendanceListData(daysOff, people, Month, Year);
+
+                // Create document generator
+                ILocalizedNames localizedNames = new TempLocalizedNames();
+                IAttendanceListDocumentGenerator documentGenerator = new AttendanceListDocumentGenerator(listData, localizedNames);
+
+                // Generate a document
+                Document document = documentGenerator.GenerateDocument();
+
+                // Get directory path and filename
+                IDirectoryProvider directoryProvider = new DirectoryProvider(localizedNames);
+                IFilenameGenerator filenameGenerator = new FilenameGenerator(localizedNames, _dateTimeProvider);
+                string path = directoryProvider.GetDocumentsDirectoryPath();
+                string filename = filenameGenerator.GeneratePdfDocumentFilename(listData);
+
+                // Save document
+                IFileSaver fileSaver = new FileSaver();
+                fileSaver.SavePdfDocument(document, path, filename);
+
+                // And open it
+                IFileOpener fileOpener = new FileOpener();
+                fileOpener.OpenFile(path, filename);
             });
         }
     }
