330dddee90977b2549e56db7d58a47a936ab13ec
Kamil Jaworski <kamilj610@gmail.com>
2019-02-21 21:03:08 +0100

    Add custom TextBox style, use it and TextToBolleanConverter

---
+++ src/AttendanceListGenerator.UI/ValueConverters/TextToBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace AttendanceListGenerator.UI.ValueConverters
+{
+    /// <summary>
+    /// Converts <see cref="string"/> from a TextBox to <see cref="bool"/>
+    /// </summary>
+    public class TextToBooleanConverter : BaseValueConverter<TextToBooleanConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // If string is null or empty return false
+            if (string.IsNullOrEmpty(value as string))
+                return false;
+
+            // If not return true
+            return true;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
