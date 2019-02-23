9fae117cef0dca225f4e3ec5c16a355dbd616892
Kamil Jaworski <kamilj610@gmail.com>
2019-02-23 22:39:14 +0100

    Add MonthToStringConverter and use it in MainWindow

---
+++ src/AttendanceListGenerator.UI/ValueConverters/MonthToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using AttendanceListGenerator.Core.Data;
+using AttendanceListGenerator.UI.Localization;
+using System;
+using System.Globalization;
+
+namespace AttendanceListGenerator.UI.ValueConverters
+{
+    public class MonthToStringConverter : BaseValueConverter<MonthToStringConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Month month))
+                return value.ToString();
+
+            LocalizedNames names = new LocalizedNames();
+
+            return names.GetMonthName(month);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
